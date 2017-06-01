648f8a6c3a8834796eb7450a7092ffee715ca89c
FALLANER-COMPUT\Alex <rinthefrosthaze>
2017-06-01 09:07:47 -0500

    Landblacking

---
+++ TankStuff/Source/TankStuff/TankStuffGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TankStuffGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKSTUFF_API ATankStuffGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
